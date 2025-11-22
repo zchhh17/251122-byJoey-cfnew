70780524e08d49be35e9c870b4169f3b5067727c
zchhh17 <zchhh17@gmail.com>
2025-11-23 05:10:32 +0800

    Delete config.sh

--- config.sh
+++
@@ -1,2 +0,0 @@
-REMOTE_SCRIPT_URL="https://raw.githubusercontent.com/zchhh17/gh-backup-script/main/gh_backup5.sh"
-REMOTE_MD5_URL="https://raw.githubusercontent.com/zchhh17/gh-backup-script/main/md5.txt"
